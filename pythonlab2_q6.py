8aaa482392b1d8a83b5bd171b1e7e1eb1a8ed61f
007jithinjose <007jithinjose@gmail.com>
2022-07-09 21:57:56 +0530

    Add files via upload

---
+++ pythonlab2_q6.py
@@ -0,0 +1,9 @@
+color_dict = {'red':'#FF0000','green':'#008000','black':'#000000','white':'#FFFFFF'}\
+#sort dictionary by key
+sort_color_dict={}
+for i in sorted(color_dict.keys()):
+    print((i, color_dict[i]), end=" ")
+ 
+    
+  
+
